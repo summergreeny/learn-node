19f59276d66e4af7cc708e81dcc2d488adf53e74
yilinliu <yilin.liu@hotmail.com>
2024-02-26 15:19:54 -0500

    quiz

--- operations/quiz/syncdelete.js
+++ operations/quiz/syncdelete.js
@@ -3,5 +3,4 @@ fs.readFile("./operations/data.txt", (err, data) => {
   if (err) console.log(err.message);
   else console.log(data);
 });
-fs.unlinkSync("./operations/data.txt");		// delete the file
-
+fs.unlinkSync("./operations/data.txt"); // delete the file
